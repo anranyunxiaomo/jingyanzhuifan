111b977c86701f486f0f87271d7a4e2ca870a6d8
anranyunxiaomo <anranyunxiaomo@users.noreply.github.com>
2026-06-29 12:35:08 +0000

    deploy: c5ff5caef47928fc8c7169cdd20c39f7a3379b42

--- flutter_bootstrap.js
+++ flutter_bootstrap.js
@@ -11,6 +11,6 @@ _flutter.buildConfig = {"engineRevision":"b8800d88be4866db1b15f8b954ab2573bba996
 
 _flutter.loader.load({
   serviceWorkerSettings: {
-    serviceWorkerVersion: "583786787"
+    serviceWorkerVersion: "4088637975"
   }
 });
