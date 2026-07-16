4167b58bc5bc86e83c9a05729aa9ec62cafce4ad
anranyunxiaomo <120796154@qq.com>
2026-07-16 10:58:32 +0800

    fix(line): whitelist yhdm_line1 to render Sakura Anime playlines on UI

--- js/app_v2.js
+++ js/app_v2.js
@@ -262,7 +262,7 @@ new Vue({
       const labelArr = this.animeDetail.player_label_arr || {};
       
       // 合法可播放的常规 M3U8 H5 线路白名单 (包含 A123 极速源与好好看黄金线)
-      const ALLOWED_KEYS = ['lzm3u8', 'wjm3u8', 'ffm3u8', 'bfzym3u8', 'hnm3u8', 'wolong', 'subm3u8', 'kym3u8', 'anich_m3u8', 'a123_line1'];
+      const ALLOWED_KEYS = ['lzm3u8', 'wjm3u8', 'ffm3u8', 'bfzym3u8', 'hnm3u8', 'wolong', 'subm3u8', 'kym3u8', 'anich_m3u8', 'a123_line1', 'yhdm_line1'];
       
       let lines = [];
       
